c7b8035adc0f427cc31b8373ee271fd0fd65e44e
naslee2 <naslee2@gmail.com>
2023-06-06 16:33:14 -0700

    added aeon hq industry

---
+++ com/fs/starfarer/api/impl/campaign/econ/impl/eusan_nation_aeon_facility.java
@@ -0,0 +1,74 @@
+package com.fs.starfarer.api.impl.campaign.econ.impl;
+
+import java.util.Random;
+
+//import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.econ.CommodityOnMarketAPI;
+import com.fs.starfarer.api.impl.campaign.ids.Commodities;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import com.fs.starfarer.api.util.Misc;
+import com.fs.starfarer.api.util.Pair;
+
+public class eusan_nation_aeon_facility extends BaseIndustry{
+
+    @Override
+    public boolean isHidden(){
+        return !market.getFactionId().equals("eusan_nation");
+    }
+
+    @Override
+    public boolean isFunctional(){
+        return super.isFunctional() && market.getFactionId().equals("eusan_nation");
+    }
+
+    public void apply(){
+
+    }
+
+    @Override
+    public void unapply(){
+        super.unapply();
+
+        unmodifyStabilityWithBaseMod();
+
+    }
+
+    protected boolean hasPostDemandSection(boolean hasDemand, IndustryTooltipMode mode) {
+		return mode != IndustryTooltipMode.NORMAL || isFunctional();
+	}
+
+	@Override
+	protected void addPostDemandSection(TooltipMakerAPI tooltip, boolean hasDemand, IndustryTooltipMode mode) {
+		if (mode != IndustryTooltipMode.NORMAL || isFunctional()) {
+			addStabilityPostDemandSection(tooltip, hasDemand, mode);
+		}
+	}
+
+    @Override
+	public String getCurrentImage() {
+		return super.getCurrentImage();
+	}
+
+    public boolean isDemandLegal(CommodityOnMarketAPI com) {
+		return true;
+	}
+
+	public boolean isSupplyLegal(CommodityOnMarketAPI com) {
+		return true;
+	}
+
+    @Override
+	public boolean isAvailableToBuild() {
+		return false;
+	}
+	
+	public boolean showWhenUnavailable() {
+		return false;
+	}
+
+	@Override
+	public boolean canImprove() {
+		return false;
+	}
+
+}
