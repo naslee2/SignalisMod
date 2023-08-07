f36098f04508179b5353c580e4b763db0319f5bc
naslee2 <naslee2@gmail.com>
2023-08-06 22:32:50 -0700

    edits

--- data/scripts/plugins/Eusan_Nation_People.java
+++ data/scripts/plugins/Eusan_Nation_People.java
@@ -8,6 +8,11 @@ import com.fs.starfarer.api.characters.PersonAPI;
 
 public class Eusan_Nation_People {
     //add people and their skills here
-
-    
+    //public String id;
+    //public String first_name;
+    //public String last_name;
+    //public FullName.Gender gender;
+    private final FactionAPI eusan_nation_faction = Global.getSector().getFaction("eusan_nation");
+    //public String rank;
+    //public String post;
 }
