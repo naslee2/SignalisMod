14e992f278664aa2ae641654d69b28b5f305db15
naslee2 <naslee2@gmail.com>
2023-11-06 15:26:22 -0800

    date settings and penrose hullmod

--- src/data/hullmods/eusan_nation_penrose_survey_equipment.java
+++ src/data/hullmods/eusan_nation_penrose_survey_equipment.java
@@ -1,5 +1,51 @@
 package data.hullmods;
 
-public class eusan_nation_penrose_survey_equipment {
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fs.starfarer.api.GameState;
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.CampaignFleetAPI;
+import com.fs.starfarer.api.combat.BaseHullMod;
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+import com.fs.starfarer.api.impl.campaign.SurveyPluginImpl;
+import com.fs.starfarer.api.impl.campaign.ids.Commodities;
+import com.fs.starfarer.api.impl.campaign.ids.Stats;
+import com.fs.starfarer.api.impl.hullmods.BaseLogisticsHullMod;
+import com.fs.starfarer.api.ui.Alignment;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import com.fs.starfarer.api.util.Misc;
+import org.magiclib.util.MagicIncompatibleHullmods;
+
+public class eusan_nation_penrose_survey_equipment extends BaseHullMod{
+
+    float BONUS = 200f;
+
+    static String detailText = Global.getSettings().getString("eusan_nation_strings", "eusan_nation_hullmodDetails");
+
     
+    public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id){
+        Float mod = 1f + (BONUS/100f);
+
+        stats.getDynamic().getMod(Stats.getSurveyCostReductionId(Commodities.HEAVY_MACHINERY)).modifyFlat(id, mod);
+		stats.getDynamic().getMod(Stats.getSurveyCostReductionId(Commodities.SUPPLIES)).modifyFlat(id, mod);
+    }
+
+    public String getDescriptionParam(int index, HullSize hullSize) {
+		if (index == 0) return "" + (int) BONUS + "%";
+		return null;
+	}
+
+    @Override
+    public void addPostDescriptionSection(TooltipMakerAPI tooltip, HullSize hullSize, ShipAPI ship, float width, boolean isForModSpec){
+        final Color flavor = new Color(110,110,110,255);
+
+        tooltip.addSectionHeading(detailText, Alignment.MID, 10.0F);
+        //tooltip.addPara(detailText, width, flavor, null)
+
+    }
+
 }
