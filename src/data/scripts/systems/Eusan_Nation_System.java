2fab53e12826afeb0f29b8e77e2aa8fca1d4cc2d
naslee2 <naslee2@gmail.com>
2024-01-28 16:03:22 -0800

    updating star system

--- src/data/scripts/systems/Eusan_Nation_System.java
+++ src/data/scripts/systems/Eusan_Nation_System.java
@@ -2,6 +2,7 @@ package data.scripts.systems;
 
 import java.awt.Color;
 import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.CustomCampaignEntityAPI;
 import com.fs.starfarer.api.campaign.JumpPointAPI;
 import com.fs.starfarer.api.campaign.PlanetAPI;
 import com.fs.starfarer.api.campaign.SectorAPI;
@@ -23,7 +24,7 @@ public class Eusan_Nation_System {
 
     public void generate(SectorAPI sector){
         StarSystemAPI system = sector.createStarSystem("Alatyr");
-        system.getLocation().set(-34000, 35000);
+        system.getLocation().set(-33000, 34000);
         system.setBackgroundTextureFilename("graphics/backgrounds/background5.jpg");
     
         PlanetAPI signalisStar = system.initStar("Die Sonne", "star_yellow", 650f, 450); //initeStar( Unique starId, planettype from planets.json, radius, corona radius from star edge)
@@ -37,15 +38,19 @@ public class Eusan_Nation_System {
         SectorEntityToken loc3 = system.addCustomEntity("signalisStar_loc3", "Nav Buoy 84-21-A", "nav_buoy", "eusan_nation"); //Date when Falke became ill
 
         //Setting stable locations setCircularOrbitPointingDown(location/star/planet, angle, orbit raidus/range, orbit period)
-        relay_eusan.setCircularOrbitPointingDown(signalisStar, 40f, 5200f, 220f);
-        loc2.setCircularOrbitPointingDown(signalisStar, 310f, 8000f, 235f);
-        loc3.setCircularOrbitPointingDown(signalisStar, 178f, 16600f, 921f); 
+        relay_eusan.setCircularOrbitPointingDown(signalisStar, 40f, 4200f, 220f);
+        loc2.setCircularOrbitPointingDown(signalisStar, 310f, 7000f, 235f);
+        loc3.setCircularOrbitPointingDown(signalisStar, 178f, 14600f, 921f); 
         
         //-BUYAN - Toxic World-
-        PlanetAPI buyan = system.addPlanet("eusan_nation_buyan", signalisStar , "Buyan", "toxic", 30f, 170f, 2000f, 225f);
+        PlanetAPI buyan = system.addPlanet("eusan_nation_buyan", signalisStar , "Buyan", "toxic", 30f, 170f, 2100f, 225f);
         ProcgenUsedNames.notifyUsed("Buyan");
         buyan.setFaction("eusan_nation");
         buyan.setCustomDescriptionId("eusan_nation_buyan_planet");
+
+        //System Gate
+        CustomCampaignEntityAPI signalis_system_gate = system.addCustomEntity("alatyr_gate", "Alatyr Gate", "inactive_gate", null);
+        signalis_system_gate.setCircularOrbit(signalisStar, 347f, 2950f, 280f);
     
         MarketAPI buyan_market = Global.getFactory().createMarket("buyan_market", buyan.getName(), 5);
         buyan_market.setPrimaryEntity(buyan);
@@ -65,17 +70,16 @@ public class Eusan_Nation_System {
         buyan_market.addIndustry(Industries.REFINING);
         buyan_market.addIndustry(Industries.MEGAPORT);
         buyan_market.addIndustry(Industries.HEAVYBATTERIES);
-        buyan_market.addIndustry(Industries.MILITARYBASE);
+        buyan_market.addIndustry(Industries.LIGHTINDUSTRY);
         buyan_market.addIndustry(Industries.BATTLESTATION);
         buyan_market.addSubmarket(Submarkets.SUBMARKET_OPEN);
         buyan_market.addSubmarket(Submarkets.SUBMARKET_BLACK);
         buyan_market.addSubmarket(Submarkets.SUBMARKET_STORAGE);
-        buyan_market.addSubmarket(Submarkets.GENERIC_MILITARY);
         buyan_market.setSurveyLevel(MarketAPI.SurveyLevel.FULL);
         sector.getEconomy().addMarket(buyan_market, false);
 
         //-VINETA AND THE SHATTERED MOON AND DEBRIS RING- Terran world? Water World?
-        PlanetAPI vineta = system.addPlanet("eusan_nation_vineta", signalisStar, "Vineta", "flooded_world", 88f, 190f, 6000f, 360f);
+        PlanetAPI vineta = system.addPlanet("eusan_nation_vineta", signalisStar, "Vineta", "flooded_world", 88f, 190f, 5000f, 360f);
         ProcgenUsedNames.notifyUsed("Vineta");
         vineta.setFaction("eusan_nation");
         vineta.getSpec().setTexture(Global.getSettings().getSpriteName("planets","vineta_planet"));
@@ -121,14 +125,21 @@ public class Eusan_Nation_System {
 
         system.addRingBand(vineta, "misc", "rings_asteroids0", 256f, 3, Color.gray, 256f, 740f, 305f, null, null);
 
+        //Wrecked Station related to Vineta
+        CustomCampaignEntityAPI wrecked_station = system.addCustomEntity("signalis_wrecked_station", "Abandoned Orbital Complex 6", "station_side02", "neutral");
+        wrecked_station.setCircularOrbitPointingDown(vineta, 45f, 900f, 55f);
+        wrecked_station.setInteractionImage("illustrations", "abandoned_station2");
+        wrecked_station.setCustomDescriptionId("signalis_wrecked_station");
+        Misc.setAbandonedStationMarket("signalis_wrecked_station_market", wrecked_station);
+        
         //Inner Jump Point related to Vineta
         JumpPointAPI innerJumpPoint = Global.getFactory().createJumpPoint("alatyr_jump_point", "Inner Jump Point");
-        innerJumpPoint.setCircularOrbit(signalisStar, 266, 4200f, 296f);
+        innerJumpPoint.setCircularOrbit(signalisStar, 266, 3200f, 296f);
         innerJumpPoint.setRelatedPlanet(vineta);
         system.addEntity(innerJumpPoint);
 
         //-KITZEH - Arid World-
-        PlanetAPI kitezh = system.addPlanet("eusan_nation_kitezh", signalisStar, "Kitezh", "arid", 310f, 120f, 8000f, 687f);
+        PlanetAPI kitezh = system.addPlanet("eusan_nation_kitezh", signalisStar, "Kitezh", "arid", 310f, 120f, 7000f, 687f);
         ProcgenUsedNames.notifyUsed("Kitezh");
         kitezh.setFaction("eusan_nation");
         kitezh.setCustomDescriptionId("eusan_nation_kitezh_planet");
@@ -165,13 +176,13 @@ public class Eusan_Nation_System {
         sector.getEconomy().addMarket(kitezh_market, false);
 
         //-AsteroidBelt- //starsector-core\graphics\planets
-        system.addAsteroidBelt(signalisStar, 1000, 10000f, 256f, 300f, 350f, Terrain.ASTEROID_BELT, "Asteroid Ring");
-        system.addRingBand(signalisStar, "misc", "rings_dust0", 256f, 0, Color.gray, 256f, 10200f, 400f);
-        system.addRingBand(signalisStar, "misc", "rings_asteroids0", 256f, 0, Color.gray, 256f, 10000f, 400f);
-        system.addRingBand(signalisStar, "misc", "rings_dust0", 256f, 2, Color.gray, 256f, 9800f, 400f);
+        system.addAsteroidBelt(signalisStar, 1000, 8000f, 256f, 300f, 350f, Terrain.ASTEROID_BELT, "Asteroid Ring");
+        system.addRingBand(signalisStar, "misc", "rings_dust0", 256f, 0, Color.gray, 256f, 8200f, 400f);
+        system.addRingBand(signalisStar, "misc", "rings_asteroids0", 256f, 0, Color.gray, 256f, 8000f, 400f);
+        system.addRingBand(signalisStar, "misc", "rings_dust0", 256f, 2, Color.gray, 256f, 7800f, 400f);
 
         //-GAS GIANT 1, DUST RINGS AND ROTFRONT-
-        PlanetAPI gas_giant1 = system.addPlanet("eusan_nation_gas_giant1", signalisStar, "Jove", "gas_giant", 234f, 400f, 12500f, 4330f);
+        PlanetAPI gas_giant1 = system.addPlanet("eusan_nation_gas_giant1", signalisStar, "Jove", "gas_giant", 234f, 400f, 9500f, 4330f);
         gas_giant1.getSpec().setTexture(Global.getSettings().getSpriteName("planets","gas_giant1"));
         gas_giant1.getMarket().addCondition(Conditions.DENSE_ATMOSPHERE);
         gas_giant1.getMarket().addCondition(Conditions.VOLATILES_TRACE);
@@ -216,7 +227,7 @@ public class Eusan_Nation_System {
         system.addRingBand(gas_giant1, "misc", "rings_dust0", 256f, 2, Color.white, 256f, 1000f, 300f);
         
         //-GAS GIANT 2, PRETTY RINGS AND HEIMAT-
-        PlanetAPI gas_giant2 = system.addPlanet("eusan_nation_gas_giant2", signalisStar, "Saturnus", "gas_giant", 90f, 370f, 14500f, 1076f);
+        PlanetAPI gas_giant2 = system.addPlanet("eusan_nation_gas_giant2", signalisStar, "Saturnus", "gas_giant", 90f, 370f, 10500f, 1076f);
         gas_giant2.getSpec().setTexture(Global.getSettings().getSpriteName("planets","gas_giant2"));
         gas_giant2.getMarket().addCondition(Conditions.DENSE_ATMOSPHERE);
         gas_giant2.getMarket().addCondition(Conditions.VOLATILES_TRACE);
@@ -264,7 +275,7 @@ public class Eusan_Nation_System {
         system.addRingBand(gas_giant2, "misc", "rings_special0", 256f, 1, Color.white, 256f, 700f, 60f);
         
         //-LENG-
-        PlanetAPI leng = system.addPlanet("eusan_nation_leng", signalisStar, "Leng", "barren", 180, 80f, 16400f, 3050f);
+        PlanetAPI leng = system.addPlanet("eusan_nation_leng", signalisStar, "Leng", "barren", 180, 80f, 12200f, 3050f);
         ProcgenUsedNames.notifyUsed("Leng");
         leng.setFaction("eusan_nation");
         leng.setCustomDescriptionId("eusan_nation_leng_planet");
