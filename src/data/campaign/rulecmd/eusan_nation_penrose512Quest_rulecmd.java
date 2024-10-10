550ef12880e7459282b76c3366be83de2cdb6679
Nathan Lee <35585589+naslee2@users.noreply.github.com>
2024-10-09 23:50:31 -0700

    updating rulecmd to show text at specific font

--- src/data/campaign/rulecmd/eusan_nation_penrose512Quest_rulecmd.java
+++ src/data/campaign/rulecmd/eusan_nation_penrose512Quest_rulecmd.java
@@ -1,21 +1,23 @@
 package data.campaign.rulecmd;
 
+import java.awt.Color;
 import java.util.List;
 import java.util.Map;
 
 import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.campaign.CampaignFleetAPI;
 import com.fs.starfarer.api.campaign.InteractionDialogAPI;
 import com.fs.starfarer.api.campaign.rules.MemoryAPI;
-import com.fs.starfarer.api.characters.PersonAPI;
 import com.fs.starfarer.api.campaign.TextPanelAPI;
+import com.fs.starfarer.api.SettingsAPI;
+import com.fs.starfarer.api.ui.Fonts;
 import com.fs.starfarer.api.ui.LabelAPI;
-import com.fs.starfarer.api.impl.campaign.rulecmd.AddRemoveCommodity;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
 import com.fs.starfarer.api.impl.campaign.rulecmd.BaseCommandPlugin;
-import com.fs.starfarer.api.impl.campaign.ids.Ranks;
 import com.fs.starfarer.api.util.Misc.Token;
 
 public class eusan_nation_penrose512Quest_rulecmd extends BaseCommandPlugin{
+     Color eusanRed = new Color(106,21,28,255);
+     String headerLine = "--------------------------------------------------------------------------------";
 
     @Override
     public boolean execute(String ruleId, InteractionDialogAPI dialog, List<Token> params, Map<String, MemoryAPI> memoryMap) {
@@ -43,13 +45,30 @@ public class eusan_nation_penrose512Quest_rulecmd extends BaseCommandPlugin{
                 TextPanelAPI text = dialog.getTextPanel();
                 break;
             case "addSystemStatusLockdown":
-                TextPanelAPI text2 = dialog.getTextPanel();
-                text2.addPara("Your briefing packet mentioned about a security keycard for Penrose vessels. It would take too much time to decrypt the security system. Recovering the keycard would also unlock many of the Penrose-512's compartments.%n");
-                text2.addPara("Lets hope it wasn't destroyed in the landing...");
+                TextPanelAPI text2b = dialog.getTextPanel();
+                TextPanelAPI text2c = dialog.getTextPanel();
+
+                // dialog.getTextPanel().beginTooltip().setParaFont("graphics/fonts/silver.fnt");
+                // dialog.getTextPanel().beginTooltip().addPara("PENROSE-512", eusanRed, 0);
+                // dialog.getTextPanel().beginTooltip().addPara("SCOUT SYSTEMS VEHICLE STATUS MONITOR.", eusanRed, 0);
+                // dialog.getTextPanel().beginTooltip().addPara("COMPARTMENT LOCKDOWN ACTIVE. SECURITY OVERRIDE NEEDED.", eusanRed, 0);
+                // dialog.getTextPanel().beginTooltip().setParaFontDefault();
+                // dialog.getTextPanel().addTooltip();
+
+                text2b.setFontVictor();
+                text2b.addPara(headerLine, eusanRed);
+                text2b.addPara("PENROSE-512", eusanRed);
+                text2b.addPara("SCOUT SYSTEMS VEHICLE STATUS MONITOR", eusanRed);
+                text2b.addPara("COMPARTMENT LOCKDOWN ACTIVE. SECURITY OVERRIDE NEEDED", eusanRed);
+                text2b.addPara(headerLine, eusanRed);
+                text2b.setFontInsignia();
+
+                text2c.addPara("Your briefing packet mentioned about a security keycard for Penrose vessels. It would take too much time to decrypt the security system. Recovering the keycard would also unlock many of the Penrose-512's compartments.");
+                text2c.addPara("Lets hope it wasn't destroyed in the landing...");
                 break;
-            case "":
+            default:
                 dialog.getTextPanel().addPara("Uh oh an error has occured!");
-                dialog.getTextPanel().addPara("No intel for code: " + ruleCall512);
+                dialog.getTextPanel().addPara("No intel for this code!");
                 break;
         }
 
