8b172434aba6cad5e5c45ad8b2973480ffe11fff
Nathan Lee <35585589+naslee2@users.noreply.github.com>
2024-11-08 22:17:04 -0800

    adding messageIntel class

---
+++ src/data/scripts/plugins/Eusan_Nation_MessageIntel.java
@@ -0,0 +1,22 @@
+package data.scripts.plugins;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.ui.LabelAPI;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import com.fs.starfarer.api.util.Misc;
+import com.fs.starfarer.api.impl.campaign.intel.MessageIntel;
+
+import org.apache.log4j.Logger;
+
+public class Eusan_Nation_MessageIntel {
+    static Logger logger = Global.getLogger(Eusan_Nation_MessageIntel.class);
+    public static void arianeMessage_neuroRepo(){
+        if (Global.getSector().getCharacterData().getMemoryWithoutUpdate().getBoolean("$officeryeong_neuralrepositorystart"))
+            logger.info("Logger Active: ref memory is active and fired!");
+
+    }
+}
