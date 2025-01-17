0f09e8467465de70e90b52ee96877d136481c5cb
Nathan Lee <35585589+naslee2@users.noreply.github.com>
2025-01-16 23:54:05 -0800

    adding sensor buff for Rotfront array

--- src/data/campaign/industries/Eusan_Nation_Rotfront_Array.java
+++ src/data/campaign/industries/Eusan_Nation_Rotfront_Array.java
@@ -1,37 +1,31 @@
 package data.campaign.industries;
 
 import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.campaign.SectorEntityToken;
+import com.fs.starfarer.api.campaign.CampaignFleetAPI;
 import com.fs.starfarer.api.campaign.StarSystemAPI;
 import com.fs.starfarer.api.campaign.econ.CommodityOnMarketAPI;
-import com.fs.starfarer.api.campaign.econ.Industry;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
-import com.fs.starfarer.api.campaign.econ.MarketConditionAPI;
+import com.fs.starfarer.api.combat.MutableStat.StatMod;
 import com.fs.starfarer.api.impl.campaign.econ.impl.BaseIndustry;
 import com.fs.starfarer.api.impl.campaign.econ.CommRelayCondition;
 import com.fs.starfarer.api.impl.campaign.ids.Commodities;
-import com.fs.starfarer.api.impl.campaign.SensorArrayEntityPlugin;
-import com.fs.starfarer.api.impl.campaign.ids.Industries;
 import com.fs.starfarer.api.ui.TooltipMakerAPI;
-import com.fs.starfarer.api.impl.campaign.ids.Stats;
 import com.fs.starfarer.api.util.Misc;
-import com.fs.starfarer.api.util.Pair;
 
 //import org.apache.log4j.Logger;
 
 import java.awt.Color;
-import java.util.List;
+//import java.util.List;
 
 public class Eusan_Nation_Rotfront_Array extends BaseIndustry{
 
-    //Logger logger = Global.getLogger(eusan_nation_Rotfront_Array.class);
+    //Logger logger = Global.getLogger(Eusan_Nation_Rotfront_Array.class);
 
     protected static float array_stability_bonus = 1.0f;
     protected static float rotfront_array_sensor_bonus = 250f;
     StarSystemAPI system = Global.getSector().getStarSystem("Alatyr");
-
     //List<MarketAPI> data = Misc.getMarketsInLocation(system);
-    
+
     @Override
     public boolean isHidden(){
         return !market.getFactionId().equals("eusan_nation");
@@ -51,12 +45,11 @@ public class Eusan_Nation_Rotfront_Array extends BaseIndustry{
 
         for(MarketAPI market : Misc.getMarketsInLocation(system, "eusan_nation")){
             CommRelayCondition relays = CommRelayCondition.get(market);
-            //logger.info("Logger Active: Market is: " + market);
             if(relays != null){
-                //logger.info("Logger Active: Relay: " + relays);
                 market.getStability().modifyFlat("eusan_nation_Rotfront_Array", array_stability_bonus, "Rotfront Array");
             }
         }
+        //logger.info("Logger active: entity is: " + entity);;
             
         if (!isFunctional()) {
 			supply.clear();
@@ -65,10 +58,24 @@ public class Eusan_Nation_Rotfront_Array extends BaseIndustry{
         
     }
 
-    //@Override
-    //public void advance(float amount) {
-        //super.advance(amount);
-    //}
+    @Override
+    public void advance(float amount) {
+        super.advance(amount);
+
+        for(CampaignFleetAPI fleet : system.getFleets()){
+//            if(fleet.isInHyperspaceTransition()){
+//                continue;
+//            }
+            if(fleet.getFaction() == Global.getSector().getFaction("eusan_nation") || fleet.getFaction().isPlayerFaction()){
+                StatMod current = fleet.getStats().getSensorRangeMod().getFlatBonus(id);
+
+                if (current == null || current.value <= rotfront_array_sensor_bonus){
+                    fleet.getStats().addTemporaryModFlat(0.1f, id, "Rotfront Array", rotfront_array_sensor_bonus, fleet.getStats().getSensorRangeMod());
+                }
+            }
+        }
+
+    }
 
     @Override
     public void unapply(){
@@ -89,8 +96,9 @@ public class Eusan_Nation_Rotfront_Array extends BaseIndustry{
 	protected void addPostDemandSection(TooltipMakerAPI tooltip, boolean hasDemand, IndustryTooltipMode mode){
 		float opad = 10f;
 		Color h = Misc.getHighlightColor();
-		tooltip.addPara("Enhances Stability to all markets", h, opad);
+		tooltip.addPara("Enhances Stability to all markets and provides sensor boost to allied fleets.", h, opad);
 		tooltip.addPara("Stability Bonus: %s", opad, h, "+" + (int) array_stability_bonus);
+        tooltip.addPara("Sensor Boost: %s", opad, h, "+" + (int) rotfront_array_sensor_bonus);
 	}
 
     public boolean isDemandLegal(CommodityOnMarketAPI com) {
