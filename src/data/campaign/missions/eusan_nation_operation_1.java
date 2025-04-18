01bfbc42474305bdb5524f9bb9bab521ef600aa5
Nathan Lee <35585589+naslee2@users.noreply.github.com>
2025-04-17 22:15:16 -0700

    adding operation1

--- src/data/campaign/missions/eusan_nation_operation_1.java
+++ src/data/campaign/missions/eusan_nation_operation_1.java
@@ -1,19 +1,88 @@
 package data.campaign.missions;
 
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.PersonImportance;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
+import com.fs.starfarer.api.characters.FullName;
+import com.fs.starfarer.api.characters.ImportantPeopleAPI;
+import com.fs.starfarer.api.characters.PersonAPI;
+import com.fs.starfarer.api.impl.campaign.ids.Factions;
+import com.fs.starfarer.api.impl.campaign.ids.Voices;
 import com.fs.starfarer.api.impl.campaign.missions.hub.HubMissionWithSearch;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import data.campaign.ids.Eusan_Nation_PeopleStrings;
+
+import java.awt.*;
 
 public class eusan_nation_operation_1 extends HubMissionWithSearch {
 
-    public enum Stage{
+    public enum Stage {
         TALK_TO_AGENT,
         LOCATE_HVT,
         RETURN_TO_HEIMAT,
         COMPLETED
     }
 
+    protected PersonAPI admiral_falke;
+    protected PersonAPI specialAgent;
+    protected PersonAPI hvt;
+    protected MarketAPI kazeron;
+    protected MarketAPI eldfell;
+
     @Override
     protected boolean create(MarketAPI createdAt, boolean barEvent) {
+
+        kazeron = Global.getSector().getEconomy().getMarket("kazeron");
+        admiral_falke = getImportantPerson(Eusan_Nation_PeopleStrings.ADMIRAL_FALKE);
+        specialAgent = getImportantPerson(Eusan_Nation_PeopleStrings.SPECIAL_AGENT);
+        hvt = getImportantPerson("eusan_nation_hvt");
+
+        if(admiral_falke == null){
+            return false;
+        }
+
+        if (!setGlobalReference("$eusan_nation_operation1_ref")) {
+            return false;
+        }
+
+        if (!setPersonMissionRef(specialAgent, "$eusan_nation_operation1_ref")) {
+            return false;
+        }
+
+        setStartingStage(Stage.TALK_TO_AGENT);
+        addSuccessStages(Stage.COMPLETED);
+
+        setStoryMission();
+
+        return true;
+    }
+    @Override
+    public boolean addNextStepText(TooltipMakerAPI info, Color tc, float pad){
+        if(currentStage == Stage.TALK_TO_AGENT){
+            info.addPara("Meet with the agent on Eldfell", tc, pad);
+            return true;
+        }
+        if(currentStage == Stage.LOCATE_HVT){
+            info.addPara("Locate the HVT at Kazeron", tc, pad);
+            return true;
+        }
+        else if(currentStage == Stage.RETURN_TO_HEIMAT){
+            info.addPara("Return to " + admiral_falke.getNameString() + ".", tc, pad);
+        }
         return false;
     }
+
+    @Override
+    public String getBaseName() {
+        return "Extract the HVT";
+    }
+
+    @Override
+    public String getPostfixForState() {
+        if (startingStage != null) {
+            return "";
+        }
+        return super.getPostfixForState();
+    }
+
 }
