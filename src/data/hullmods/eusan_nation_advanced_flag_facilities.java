87ceab8f3506e663124d5896809f5e93b1dbf55e
Nathan Lee <35585589+naslee2@users.noreply.github.com>
2025-05-12 19:12:59 -0700

    expanding the ops center replacement hullmod a bit for drafts.

--- src/data/hullmods/eusan_nation_advanced_flag_facilities.java
+++ src/data/hullmods/eusan_nation_advanced_flag_facilities.java
@@ -11,20 +11,32 @@ import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
 import com.fs.starfarer.api.impl.campaign.ids.HullMods;
 import com.fs.starfarer.api.impl.campaign.ids.Stats;
+import com.fs.starfarer.api.ui.Alignment;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import com.fs.starfarer.api.util.Misc;
 import org.magiclib.util.MagicIncompatibleHullmods;
+
+import java.awt.*;
 
 public class eusan_nation_advanced_flag_facilities extends BaseHullMod{
 
     public final float RECOVERY_BONUS = 225f;
 	public final String MOD_ID = "eusan_nation_advanced_flag_facilities";
 
+	String detailText = Global.getSettings().getString("eusan_nation_strings", "eusan_nation_hullmodDetails");
+	String incompatibilitiesText = Global.getSettings().getString("eusan_nation_strings", "eusan_nation_hullmodIncompatibilities");
+
     public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
 	}
 
 	public void applyEffectsAfterShipCreation(ShipAPI ship, String id){
+		magicIncompatibleHullmodsChecker(ship);
+	}
+
+	public void magicIncompatibleHullmodsChecker(ShipAPI ship) {
 		if(ship.getVariant().getHullMods().contains(HullMods.OPERATIONS_CENTER)){
 			//if someone tries to install incompatible hullmods, remove it.
-			MagicIncompatibleHullmods.removeHullmodWithWarning(ship.getVariant(), HullMods.OPERATIONS_CENTER, "eusan_nation_advanced_flag_facilities");
+			MagicIncompatibleHullmods.removeHullmodWithWarning(ship.getVariant(), HullMods.OPERATIONS_CENTER, MOD_ID);
 		}
 	}
 
@@ -63,11 +75,30 @@ public class eusan_nation_advanced_flag_facilities extends BaseHullMod{
 	}
 
 	@Override
+	public void addPostDescriptionSection(final TooltipMakerAPI tooltip, final ShipAPI.HullSize hullSize, final ShipAPI ship, final float width, final boolean isForModSpec){
+		final Color green = new Color(55,245,65,255);
+		final Color red = new Color(245,55,65,255);
+		final Color flavor = new Color(110,110,110,255);
+		final Color negative = Misc.getNegativeHighlightColor();
+		final Color negativeBG = new Color(128,38,0,175);
+		final float pad5 = 5.0f;
+		final float pad10 = 10.0f;
+
+		//Details section
+		tooltip.addSectionHeading(detailText, Alignment.MID, pad10);
+
+		//Incompatibilities
+		tooltip.addSectionHeading(incompatibilitiesText, negative,negativeBG, Alignment.MID, pad10);
+
+		//Quotes
+	}
+
+	@Override
 	public boolean isApplicableToShip(ShipAPI ship){
 		if (ship != null && ship.getVariant().getHullMods().contains(HullMods.OPERATIONS_CENTER)){
 			return false;
 		}
-		if (ship != null && ship.getHullSpec().getHullId().contains("eusan_nation_admiral")){
+		if (ship != null && ship.getHullSpec().getHullId().startsWith("eusan_nation_")){
 			return true;
 		}
 		return false;
