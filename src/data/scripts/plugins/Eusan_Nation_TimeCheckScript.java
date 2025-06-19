66b3a25af0d954f43c52a0dadc5fb7259f584b92
Nathan Lee <35585589+naslee2@users.noreply.github.com>
2025-06-18 19:44:38 -0700

    pushing a test script

--- src/data/scripts/plugins/Eusan_Nation_TimeCheckScript.java
+++ src/data/scripts/plugins/Eusan_Nation_TimeCheckScript.java
@@ -11,7 +11,7 @@ import java.util.GregorianCalendar;
 
 public class Eusan_Nation_TimeCheckScript implements EveryFrameScript {
 
-    Logger logger = Global.getLogger(Eusan_Nation_TimeCheckScript.class);
+    //Logger logger = Global.getLogger(Eusan_Nation_TimeCheckScript.class);
     //protected float daysElapsed = 0f;
     private IntervalUtil dayInterval = new IntervalUtil(1f, 1f);
 
@@ -31,7 +31,7 @@ public class Eusan_Nation_TimeCheckScript implements EveryFrameScript {
         //int days = clock.getCycle();
         //int days = clock.getHour();
         //GregorianCalendar days = clock.getCal();
-        logger.info("Logger active. data: " + days); //java.util.GregorianCalendar[time=-55661212942000,areFieldsSet=true,areAllFieldsSet=true,lenient=true,zone=sun.util.calendar.ZoneInfo[id="America/Los_Angeles",offset=-28800000,dstSavings=3600000,useDaylight=true,transitions=185,lastRule=java.util.SimpleTimeZone[id=America/Los_Angeles,offset=-28800000,dstSavings=3600000,useDaylight=true,startYear=0,startMode=3,startMonth=2,startDay=8,startDayOfWeek=1,startTime=7200000,startTimeMode=0,endMode=3,endMonth=10,endDay=1,endDayOfWeek=1,endTime=7200000,endTimeMode=0]],firstDayOfWeek=1,minimalDaysInFirstWeek=1,ERA=1,YEAR=206,MONTH=2,WEEK_OF_YEAR=10,WEEK_OF_MONTH=2,DAY_OF_MONTH=2,DAY_OF_YEAR=61,DAY_OF_WEEK=1,DAY_OF_WEEK_IN_MONTH=1,AM_PM=1,HOUR=3,HOUR_OF_DAY=15,MINUTE=57,SECOND=38,MILLISECOND=0,ZONE_OFFSET=-28800000,DST_OFFSET=0]
+        //logger.info("Logger active. data: " + days); //java.util.GregorianCalendar[time=-55661212942000,areFieldsSet=true,areAllFieldsSet=true,lenient=true,zone=sun.util.calendar.ZoneInfo[id="America/Los_Angeles",offset=-28800000,dstSavings=3600000,useDaylight=true,transitions=185,lastRule=java.util.SimpleTimeZone[id=America/Los_Angeles,offset=-28800000,dstSavings=3600000,useDaylight=true,startYear=0,startMode=3,startMonth=2,startDay=8,startDayOfWeek=1,startTime=7200000,startTimeMode=0,endMode=3,endMonth=10,endDay=1,endDayOfWeek=1,endTime=7200000,endTimeMode=0]],firstDayOfWeek=1,minimalDaysInFirstWeek=1,ERA=1,YEAR=206,MONTH=2,WEEK_OF_YEAR=10,WEEK_OF_MONTH=2,DAY_OF_MONTH=2,DAY_OF_YEAR=61,DAY_OF_WEEK=1,DAY_OF_WEEK_IN_MONTH=1,AM_PM=1,HOUR=3,HOUR_OF_DAY=15,MINUTE=57,SECOND=38,MILLISECOND=0,ZONE_OFFSET=-28800000,DST_OFFSET=0]
 
         //int days = clock.getDay(); //return float number. Example 3.0
 
